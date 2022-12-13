e5f039fed85ae7aa17bc2d3818737efdbd225a4e
Josemi <josemiguel2201@gmail.com>
2022-12-13 21:49:54 +0100

    Realizar integracion con netlify y resolver error fotos

---
+++ functions/api.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const serverless = require('serverless-http');
+const cors = require('cors');
+
+const app = express()
+
+app.use(cors())
+app.use( express.json())
+
+//Rutas
+const baseRoute = "/api"
+app.use(`${baseRoute}/product`, require('../routes/product.js'));
+app.use(`${baseRoute}/cart`, require('../routes/cart.js'));
+
+module.exports.handler = serverless(app);
